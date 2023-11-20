3495799d91dfac16b8f26436c163c904c926331c
Debbroto Kumar Karmokar <136155528+debbrotokumark@users.noreply.github.com>
2023-11-20 12:03:51 +0600

    Add files via upload

---
+++ functionoverloding.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void fun(int a){
+cout<<"fun void\n ";
+
+
+}
+
+double fun(double x){
+
+cout<<"fun double\n";
+
+}
+int main()
+{
+
+
+fun(2.0);
+//fun(4);
+
+    return 0;
+}
+
+
